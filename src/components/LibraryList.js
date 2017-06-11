c7517ce33299a9ca4dd8a538c2086899c626085b
unknown <sdak006@gmail.com>
2017-06-11 16:08:55 -0500

    added listitem

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({ 
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow(library) {
+        return <ListItem library={library} />;
+    }
+
     render () {
-        return;
+        return (
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 }
 
-export default LibraryList;
+//return sets the props for the class
+const mapStateToProps = state => {
+    return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
 
 export default combineReducers({
-    //list of libraries to return to the user
     //key       //reducer
     libraries: LibraryReducer
 
